0fde1c7d1a3c00890ee9c310c5106069d6d3ea95
f <f>
2019-11-25 18:41:39 -0500

    modulo usuarios

--- src/main/java/com/clinica/controller/UsuarioController.java
+++ src/main/java/com/clinica/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.clinica.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,12 +44,37 @@ public class UsuarioController {
 			} else {
 				usuario.setUsername(usuario.getUsername());
 				usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-				usuario.setCargo("paciente");
+				usuario.setCargo(usuario.getCargo());
 				usuario.addAuthority("USER");
 				usuario.setEnable(true);
 				usuarioService.save(usuario);
 			}
 		} catch(Exception e) {System.out.println(e.getMessage());}
 		return "login";
+	}
+	
+	@GetMapping("lista")
+	public String ListarUsuarios(Model model) {
+		try {
+			List<Usuario> usuarios = usuarioService.findAll();
+			model.addAttribute("usuarios",usuarios);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return "usuario/listaUsuarios";
+	}
+	
+	@GetMapping("delete/{id}")
+	public String deleteUser(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Usuario> usuario = usuarioService.findById(id);
+			if(usuario.isPresent()) {
+				usuarioService.deleteById(id);
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			return "error";
+		}
+		return "redirect:/usuario/lista";
 	}
 }
